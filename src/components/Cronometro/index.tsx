eefe47063830ff7b1dbac137b95d63ae8e633eab
kaworu <luishenriqueribeiro3@gmail.com>
2022-11-28 22:18:53 -0300

    adicionando relogio e cronometro e seus respectivos css

---
+++ src/components/Cronometro/index.tsx
@@ -0,0 +1,19 @@
+import Botao from '../Botao/index'
+import  Relogio  from './Relogio';
+import style from './Cronometro.module.scss'
+
+
+export default function Cronometro() {
+    return (
+    <div className={style.cronometro}>
+        <p className={style.titulo}>Escolha um card e inicie o cronometro</p>
+        <div className={style.relogioWrapper}>
+            <Relogio/>
+        </div>
+        <Botao
+        texto='Começar'
+        />
+    </div>
+    
+    )   
+}
