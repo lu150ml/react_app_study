99f3344f51259a82b308dd5b50e857acc5ded632
kaworu <luishenriqueribeiro3@gmail.com>
2022-12-02 23:55:40 -0300

    criando a função dos botões e  adicionando a função setState

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Lista.module.scss';
 import Item from './Item/index';
 
 function Lista() {
-  const tarefas = [{
-    tarefa: 'React',
-    tempo: '02:00:00'
-  }, {
-    tarefa: 'Javascript',
-    tempo: '01:00:00'
-  }, {
-    tarefa: "Typescript",
-    tempo: "03:00:00"
-  }];
+  const [tarefas, setTarefas] = useState(
+    [{
+      tarefa: 'React',
+      tempo: '02:00:00'
+    }, {
+      tarefa: 'Javascript',
+      tempo: '01:00:00'
+    }, {
+      tarefa: "Typescript",
+      tempo: "03:00:00"
+    }]);
   return (
     <aside className={style.listaTarefas}>
-      <h2> Estudos do dia </h2>
+      <h2 onClick={()=>{
+        setTarefas([...tarefas,{ tarefa:"Etudar estado", tempo: "05:00:00"}])
+      }}> Estudos do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item 
